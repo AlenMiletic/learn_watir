e5ce1c7b7abd41542da12f38c37636695f363199
Alen Miletic <alenmiletic@tentamen.local>
2017-08-16 18:52:46 +0200

    Firefox version with methods

--- sixth_script.rb
+++ sixth_script.rb
@@ -1,5 +1,5 @@
 require 'watir'
-@browser = Watir::Browser.new :chrome
+@browser = Watir::Browser.new :firefox
 
 def goto_puppy_site
   @browser.goto "puppies.herokuapp.com"
@@ -35,5 +35,5 @@ adopt_puppy_number 1
 adopt_another_puppy
 adopt_puppy_number 2
 checkout("Alen", "Ul.Grabrovec 151 \n49210 Zabok", "alen.miletic6@gmail.com", "Credit card")
-veify("Thank you for adopting a puppy!")
+verify("Thank you for adopting a puppy!")
 close_browser
