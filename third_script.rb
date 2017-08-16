44e91a4ee32f59a9def9c35dae26165fdebf92aa
Alen Miletic <alenmiletic@tentamen.local>
2017-08-16 18:07:44 +0200

    First automated adoption of a puppys

--- third_script.rb
+++ third_script.rb
@@ -1,17 +1,12 @@
 require 'watir'
 browser = Watir::Browser.new :chrome
 browser.goto 'puppies.herokuapp.com'
-sleep 5
 browser.button(value: "View Details", index: 1).click
-sleep 2
 browser.button(value: "Adopt Me!").click
-sleep 2
 browser.button(value: "Complete the Adoption").click
-sleep 2
 browser.text_field(id: "order_name").set("Alen")
 browser.textarea(id: "order_address").set("Ul.Grabrovec 151 \n 49210 Zabok")
 browser.text_field(id: "order_email").set("alen.miletic6@gmail.com")
 browser.select_list(id: "order_pay_type").select("Credit card")
 browser.button(value: "Place Order").click
-sleep 5
 browser.close
