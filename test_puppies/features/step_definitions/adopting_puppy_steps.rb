610a7bb5b060234d019a17585ddd8bb59a4ef356
Alen Miletic <alen.miletic6@gmail.com>
2017-08-23 17:09:20 +0100

    Added Page Object and methods

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -1,37 +1,35 @@
-def row_for(line)
-  (line -1) * 6
-end
-
-def cart_line_item(line)
-  @browser.table(index: 0)[row_for(line)]
-end
-
-
 Given(/^I am on the puppy adoption site$/) do
   @browser.goto "http://puppies.herokuapp.com"
 end
 
 When(/^I click the first View Details button$/) do
+  sleep 2
   @browser.button(value: "View Details").click
 end
 
 When(/^I click the Adopt Me button$/) do
+  sleep 2
   @browser.button(value: "Adopt Me!").click
+  @cart = ShoppingCartPage.new(@browser)
 end
 
 When(/^I click the Adopt Another Puppy button$/) do
+  sleep 2
   @browser.button(value: "Adopt Another Puppy").click
 end
 
 When(/^I click the second View Details button$/) do
+  sleep 2
   @browser.button(value: "View Details", index: 1).click
 end
 
 When(/^I click the Complete the Adoption button$/) do
+  sleep 2
   @browser.button(value: "Complete the Adoption").click
 end
 
 When(/^I enter "([^"]*)" in the name field$/) do |name|
+  sleep 2
   @browser.text_field(id: "order_name").set(name)
 end
 
@@ -48,23 +46,24 @@ When(/^I select "([^"]*)" from the pay dropdown$/) do |pay_type|
 end
 
 When(/^I click the Place Order button$/) do
+  sleep 2 
   @browser.button(value: "Place Order").click
 end
 
 Then(/^I should see "([^"]*)"$/) do |expected|
+  sleep 2
   expect(@browser.text).to include expected
 end
 
-Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line|
-  row = (line.to_i - 1) * 6
-  expect(cart_line_item(line.to_i)[1].text).to include name
+Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line_item|
+  sleep 2
+  expect(@cart.name_for_line_item(line_item.to_i)).to include name
 end
 
-Then(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |price, line|
-  row = (line.to_i - 1) * 6
-  expect(cart_line_item(line.to_i)[3].text).to eql price
+Then(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |price, line_item|
+  expect(@cart.subtotal_for_line_item(line_item.to_i)).to eql price
 end
 
 Then(/^I should see "([^"]*)" as the cart total$/) do |total|
-  expect(@browser.td(class: "total_cell").text).to eql total
+  expect(@cart.cart_total).to eql total
 end
