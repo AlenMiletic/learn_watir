873d68cc2ed8d96bfb44de21e6bb1c24f5b63c36
Alen Miletic <alenmiletic@tentamen.local>
2018-02-28 17:07:39 +0100

    refactored web services test code

--- test_puppies/features/step_definitions/validating_usa_zip_code.rb
+++ test_puppies/features/step_definitions/validating_usa_zip_code.rb
@@ -13,10 +13,10 @@ end
 Then("I should get the following information:") do |table|
   expected = table.hashes.first
   using(ZipCodeInformationService) do |service|
-    expect(service.city).to eql expected['city']
-    expect(service.state).to eql expected['state']
-    expect(service.area_code).to eql expected['area_code']
-    expect(service.time_zone).to eql expected['time_zone']
+    expect(service.response_for(:city)).to eql expected['city']
+    expect(service.response_for(:state)).to eql expected['state']
+    expect(service.response_for(:area_code)).to eql expected['area_code']
+    expect(service.response_for(:time_zone)).to eql expected['time_zone']
   end
 end
 
--- test_puppies/lib/services/zip_code_information_service.rb
+++ test_puppies/lib/services/zip_code_information_service.rb
@@ -3,20 +3,8 @@ class ZipCodeInformationService
 
   wsdl 'http://www.webservicex.net/uszip.asmx?WSDL'
 
-  def city
-    message[:city]
-  end
-
-  def state
-    message[:state]
-  end
-
-  def area_code
-    message[:area_code]
-  end
-
-  def time_zone
-    message[:time_zone]
+  def response_for(key)
+    message[key]
   end
 
   private 
