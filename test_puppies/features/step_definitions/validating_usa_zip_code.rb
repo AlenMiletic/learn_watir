fb624cb49cacc37f27a8977dd6ccd4a09e4c4781
Alen Miletic <alenmiletic@tentamen.local>
2018-02-28 16:53:06 +0100

    testing web services with soap object gem

---
+++ test_puppies/features/step_definitions/validating_usa_zip_code.rb
@@ -0,0 +1,22 @@
+When("I ask the service for the supported operations") do
+ @operations = using(ZipCodeInformationService).operations
+end
+
+Then("{string} should be supported") do |operation|
+  expect(@operations).to include operation.to_sym
+end
+
+When("I ask for the zip code information for {string}") do |zip_code|
+  using(ZipCodeInformationService).get_info_by_zip('USZip' => zip_code)
+end
+
+Then("I should get the following information:") do |table|
+  expected = table.hashes.first
+  using(ZipCodeInformationService) do |service|
+    expect(service.city).to eql expected['city']
+    expect(service.state).to eql expected['state']
+    expect(service.area_code).to eql expected['area_code']
+    expect(service.time_zone).to eql expected['time_zone']
+  end
+end
+
--- test_puppies/features/support/env.rb
+++ test_puppies/features/support/env.rb
@@ -5,8 +5,10 @@ require 'require_all'
 require 'factory_girl'
 require 'nokogiri'
 require 'builder'
+require 'soap-object'
 
 require_all 'lib'
 
 World(PageObject::PageFactory)
 World(FactoryGirl::Syntax::Methods)
+World(SoapObject::Factory)
