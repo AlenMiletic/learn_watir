dd083d101089164f1b673a097b16fd11a65ede67
Alen Miletic <alenmiletic@tentamen.local>
2018-03-12 16:37:48 +0100

    Added a Scenario for testing JavScript alert popup and how to handle an alert popup

--- test_puppies/features/step_definitions/web01_steps.rb
+++ test_puppies/features/step_definitions/web01_steps.rb
@@ -9,3 +9,13 @@ end
 Then("the receiver should have {string}") do |expected|
   expect(on(FramesPage).receiver).to eql expected
 end
+
+When("I popup the alert") do
+  on(FramesPage) do |page| 
+    @alert_text = page.alert_text
+  end
+end
+
+Then("the text from the alert should read {string}") do |expected|
+  expect(@alert_text).to eql expected
+end
--- test_puppies/lib/pages/frames_page.rb
+++ test_puppies/lib/pages/frames_page.rb
@@ -16,4 +16,13 @@ class FramesPage
     self.sender = message
     send_to_receiver
   end
+
+  def alert_text
+    in_frame(:id => 'frame_3') do |frame|
+      @alert_text = alert(frame) do
+        button_element(:id => 'alert_button', :frame => frame).click
+      end
+    end
+    @alert_text
+  end
 end
